0261a78194fd7d6eb3964cade862b86f25dcc8fb
osni junior <osniandrade@gmail.com>
2021-05-01 12:50:45 +0000

    added errno to ft_read

--- ft_read.s
+++ ft_read.s
@@ -1,14 +1,19 @@
 section     .text
     global  ft_read
+    extern  __errno_location
 
 ft_read:
 
     mov     rax, 0
     syscall
     cmp     rax, 0
-    jl      _finish
+    jl      _error
     ret
 
-_finish:
+_error:
+    neg     rax
+    mov     rdi, rax
+    call    __errno_location
+    mov     [rax], rdi
     mov     rax, -1
     ret
