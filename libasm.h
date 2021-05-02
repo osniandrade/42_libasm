0b3289947c51db894b9b88fcc389c41dbb0a6c21
osni junior <osniandrade@gmail.com>
2021-05-02 12:42:09 +0000

    branch for project delivery at 42

--- libasm.h
+++ libasm.h
@@ -1,11 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libasm.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ocarlos- <ocarlos-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/18 09:05:02 by ocarlos-          #+#    #+#             */
+/*   Updated: 2021/01/01 16:26:04 by ocarlos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBASM_H
 # define LIBASM_H
 
 # include <stdio.h>
 # include <unistd.h>
-# include <string.h>
 # include <fcntl.h>
-# include <stdlib.h>
 # include <errno.h>
 
 ssize_t ft_read(int fd, void *buf, size_t nbyte);
