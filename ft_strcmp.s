ba6af8025ddcdc9a1c3a5c21331e14d44cf66d35
osni junior <osniandrade@gmail.com>
2021-05-10 16:55:37 +0000

    small update on ft_strcmp

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -14,21 +14,22 @@ section     .text
     global  ft_strcmp
 
 ft_strcmp:
+    xor     rax, rax
+    xor     rbx, rbx
     mov     r8, -1
 
 _while:
     inc     r8
     mov     al, byte [rdi + r8]
-    mov     bl, byte [rsi + r8]
-    cmp     al, bl
-    jne     _finish
+    mov     dl, byte [rsi + r8]
     cmp     al, 0
     je      _finish
-    cmp     bl, 0
+    cmp     dl, 0
     je      _finish
+    cmp     al, dl
+    jne     _finish
     je      _while
 
 _finish:
-    sub     al, bl
-    movsx   rax, al
+    sub     rax, rdx
     ret
